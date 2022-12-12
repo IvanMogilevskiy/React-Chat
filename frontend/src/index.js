954d432b51aed894fb72bd0f56ab6adbbf554f2b
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-12-12 19:05:43 +0300

    add router, login, root, errorPage

---
+++ frontend/src/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-param-reassign, no-console  */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Root from './routes/root.jsx';
+import ErrorPage from './error-page.jsx';
+import Login from './routes/login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
