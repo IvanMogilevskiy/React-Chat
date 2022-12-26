5ff948351d0fbd58b25f2421d0926eac20f4161d
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-12-26 18:47:15 +0300

    edit file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,32 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import App from './components/App.js';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Root from './routes/root.jsx';
-import ErrorPage from './error-page.jsx';
-import Login from './routes/login.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/login',
-        element: <Login />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
