62f6d1a4e3dedeaa0cf0c6116d83772c6c067e48
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2022-12-26 18:48:27 +0300

    add LoginPage.jsx

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,122 @@
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { Button, Form } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
+import useAuth from '../hooks/useAuth.jsx';
+import loginLogo from '../images/login.jpeg';
+import routes from './routes.js';
+
+const LoginPage = () => {
+  const auth = useAuth();
+  const [authFailed, setAuthFailed] = useState(false);
+  const inputRef = useRef();
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: yup.object({
+      username: yup.string().required('Required'),
+      password: yup.string().required('Required'),
+    }),
+    onSubmit: async (values) => {
+      setAuthFailed(false);
+
+      try {
+        const response = await axios.post(routes.loginPath(), values);
+        localStorage.setItem('user', JSON.stringify(response.data));
+        auth.logIn();
+        const { from } = location.state || { from: { pathname: '/' } };
+        navigate(from);
+      } catch (err) {
+        formik.setSubmitting(false);
+        if (err.isAxiosError && err.response.status === 401) {
+          setAuthFailed(true);
+          inputRef.current.select();
+          return;
+        }
+        throw err;
+      }
+    },
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body row p-5">
+              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                <img src={loginLogo} className="rounded-circle" alt="Войти" />
+              </div>
+              <Form
+                onSubmit={formik.handleSubmit}
+                className="col-12 col-md-6 mt-3 mt-mb-0"
+              >
+                <h1 className="text-center mb-4">Войти</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Label htmlFor="username">Ваш ник</Form.Label>
+                  <Form.Control
+                    name="username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    autoComplete="username"
+                    required
+                    placeholder="Ваш ник"
+                    id="username"
+                    className="form-control"
+                    isInvalid={authFailed}
+                    ref={inputRef}
+                  />
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Label className="form-label" htmlFor="password">
+                    Пароль
+                  </Form.Label>
+                  <Form.Control
+                    name="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    autoComplete="current-password"
+                    required
+                    placeholder="Пароль"
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    isInvalid={authFailed}
+                    ref={inputRef}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    the username or password is incorrect
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                  type="submit"
+                  className="w-100 mb-3 btn btn-outline-primary"
+                >
+                  Войти
+                </Button>
+              </Form>
+            </div>
+            <div className="card-footer p-4">
+              <div className="text-center">
+                <span>Нет аккаунта?</span>
+                <a href="/signup">Регистрация</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
