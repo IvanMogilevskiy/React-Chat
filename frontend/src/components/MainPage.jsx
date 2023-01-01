2b9725ace1035f7ff885b53ee4e197bde5c9751d
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-01 22:04:19 +0300

    add MainPage.jsx

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import useAuth from '../hooks/useAuth.jsx';
+import { fetchData } from '../slices/chatSlice.js';
+import ChannelArea from './ChannelArea.jsx';
+import MessageArea from './MessageArea.jsx';
 
-const MainPage = () => {};
+const MainPage = () => {
+  const dispatch = useDispatch();
+  const { getAuthHeader } = useAuth();
+  const header = getAuthHeader();
+
+  useEffect(() => {
+    dispatch(fetchData(header));
+  }, [dispatch, header]);
+
+  return (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <ChannelArea />
+        <MessageArea />
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
