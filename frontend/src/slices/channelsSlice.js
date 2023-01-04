b67afe7872f2d2fc56f8586eceeb2c51fb8e5b8b
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-04 14:04:41 +0300

    add channelsSlice

---
+++ frontend/src/slices/channelsSlice.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { fetchData } from './chatSlice.js';
+
+const channelsAdapter = createEntityAdapter();
+const initialState = channelsAdapter.getInitialState({
+  currentChannelId: null,
+  defaultChannelId: 1,
+});
+
+const channelsSlice = createSlice({
+  name: 'channels',
+  initialState,
+  reducers: {
+    setCurrentChannel: (state, action) => {
+      state.currentChannelId = action.payload;
+    },
+    addChannel: channelsAdapter.addOne,
+    removeChannel: (state, action) => {
+      if (state.currentChannelId === action.payload) {
+        state.currentChannelId = state.defaultChannelId;
+      }
+      channelsAdapter.removeOne(state, action.payload);
+    },
+    renameChannel: channelsAdapter.updateOne,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.fulfilled, (state, {
+        payload,
+      }) => {
+        channelsAdapter.addMany(state, payload.channels);
+        state.currentChannelId = payload.currentChannelId;
+      });
+  },
+});
+
+export const {
+  setCurrentChannel,
+  addChannel,
+  removeChannel,
+  renameChannel,
+} = channelsSlice.actions;
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
+export default channelsSlice.reducer;
