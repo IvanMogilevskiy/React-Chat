12420d46c4fb9ba7b71402589a927d6e6539b4c3
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-09 20:37:25 +0300

    add modalsSlice.js

---
+++ frontend/src/slices/modalsSlice.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const modalsSlice = createSlice({
+  name: 'modals',
+  initialState: {
+    type: null,
+    item: null,
+  },
+  reducers: {
+    openModal: (state, { payload }) => {
+      state.type = payload.type;
+      state.item = payload.item;
+    },
+    hideModal: (state) => {
+      state.type = null;
+      state.item = null;
+    },
+  },
+});
+
+export const { openModal, hideModal } = modalsSlice.actions;
+export default modalsSlice.reducer;
