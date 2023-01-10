1d5ee82112fa277182f01f1d73934cfc58077e56
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-11 00:08:03 +0300

    add ru.js

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,53 @@
+export default {
+  translation: {
+    login: {
+      username: 'Ваш ник',
+      password: 'Пароль',
+      login: 'Войти',
+      wrongAuthData: 'Неверные имя пользователя или пароль',
+      required: 'Обязательное поле',
+      noAcc: 'Нет аккаунта? ',
+      linkText: 'Регистрация',
+    },
+    signup: {
+      registration: 'Регистрация',
+      confirmPassword: 'Подтвердите пароль',
+      toSignUp: 'Зарегистрироваться',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      usernameLength: 'От 3 до 20 символов',
+      minPasswordLength: 'Не менее 6 символов',
+      passwordsShouldMatch: 'Пароли должны совпадать',
+      required: 'Обязательное поле',
+
+    },
+    chat: {
+      send: 'Отправить',
+    },
+    notifications: {
+      connectionError: 'Ошибка соединения',
+      channelCreated: 'Канал создан',
+      channelRenamed: 'Канал переименован',
+      channelRemoved: 'Канал удалён',
+    },
+    channels: {
+      control: 'Управление каналом',
+      remove: 'Удалить',
+      rename: 'Переименовать',
+      channels: 'Каналы',
+    },
+    errorPage: {
+      wrongPage: 'Страница не найдена',
+      goBack: 'Но вы можете перейти ',
+      linkText: 'на главную страницу',
+    },
+    navbar: {
+      logout: 'Выйти',
+    },
+    messages: {
+      messages_one: '{{ count }} сообщение',
+      messages_few: '{{ count }} сообщения',
+      messages_many: '{{ count }} сообщений',
+    },
+  },
+};
