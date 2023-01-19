4dd9f81f5181650df8abf8725469f100bc7b699d
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-19 18:39:43 +0300

    add scroll

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -12,15 +12,26 @@ import useSocket from '../hooks/useSocket.jsx';
 const MessageArea = () => {
   const { t } = useTranslation();
   const inputRef = useRef();
-  useEffect(() => inputRef.current.focus(), []);
 
   const currentChannelId = useSelector(
     (state) => state.channels.currentChannelId,
   );
+
+  useEffect(() => inputRef.current.focus());
+
   const messages = useSelector(messagesSelectors.selectAll);
   const currentMessages = messages.filter(
     (msg) => msg.channelId === currentChannelId,
   );
+
+  const latestMessageRef = useRef();
+
+  useEffect(() => {
+    latestMessageRef.current.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }, [currentMessages]);
+
   const currentChannel = useSelector((state) => channelsSelectors
     .selectById(state, currentChannelId));
   const username = JSON.parse(localStorage.getItem('username'));
@@ -61,7 +72,7 @@ const MessageArea = () => {
             {t('messages.messages', { count: messageCount })}
           </span>
         </div>
-        <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+        <div id="messages-box" className="chat-messages overflow-auto px-5">
           {currentMessages.map((msg) => (
             <div className="text-break mb-2" key={msg.id}>
               <b>{msg.username}</b>
@@ -69,46 +80,47 @@ const MessageArea = () => {
               {msg.message}
             </div>
           ))}
-          <div className="mt-auto px-5 py-3">
-            <Form
-              noValidate
-              className="py-1 border rounded-2"
-              onSubmit={formik.handleSubmit}
-            >
-              <div className="input-group has-validation">
-                <Form.Control
-                  onChange={formik.handleChange}
-                  name="message"
-                  id="message"
-                  aria-label={t('messages.newMessage')}
-                  placeholder={t('messages.enterMessage')}
-                  className="border-0 p-0 ps-2"
-                  value={formik.values.message}
-                  ref={inputRef}
-                />
-                <Button
-                  type="submit"
-                  disabled={formik.values.message === ''}
-                  className="btn btn-group-vertical"
+          <span ref={latestMessageRef} />
+        </div>
+        <div className="mt-auto px-5 py-3">
+          <Form
+            noValidate
+            className="py-1 border rounded-2"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="input-group has-validation">
+              <Form.Control
+                onChange={formik.handleChange}
+                name="message"
+                id="message"
+                aria-label={t('messages.newMessage')}
+                placeholder={t('messages.enterMessage')}
+                className="border-0 p-0 ps-2"
+                value={formik.values.message}
+                ref={inputRef}
+              />
+              <Button
+                type="submit"
+                disabled={formik.values.message === ''}
+                className="btn btn-group-vertical"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      /* eslint-disable-next-line  */
-                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                    />
-                  </svg>
-                  <span className="visually-hidden">{t('messages.send')}</span>
-                </Button>
-              </div>
-            </Form>
-          </div>
+                  <path
+                    fillRule="evenodd"
+                    /* eslint-disable-next-line  */
+                    d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                  />
+                </svg>
+                <span className="visually-hidden">{t('messages.send')}</span>
+              </Button>
+            </div>
+          </Form>
         </div>
       </div>
     </Col>
