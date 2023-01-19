6df38b69fa91b567b94b4830b28e16aa1e9493d7
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-19 18:34:52 +0300

    add chatSlice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice.js';
 import messagesReducer from './messagesSlice.js';
 import modalsReducer from './modalsSlice.js';
+import chatReducer from './chatSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
     messages: messagesReducer,
     modals: modalsReducer,
+    chat: chatReducer,
   },
 });
