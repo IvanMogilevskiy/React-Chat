5a9279ce21b4b270b74506cfb73cddc62543b62c
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-01-30 21:17:02 +0300

    add apiPath.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,6 @@
-const apiPath = '/api/v1';
-
 export default {
-  loginPath: () => [apiPath, 'login'].join('/'),
-  signUpPath: () => [apiPath, 'signup'].join('/'),
-  usersPath: () => [apiPath, 'data'].join('/'),
-  loginPage: () => '/login',
-  mainPage: () => '/',
-  signUpPage: () => '/signup',
-  errorPage: () => '*',
+  loginPage: '/login',
+  mainPage: '/',
+  signUpPage: '/signup',
+  errorPage: '*',
 };
